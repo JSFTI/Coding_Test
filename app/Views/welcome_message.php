0f6693652cd7f304ebb9f9dfafd4856339eb3647
JSFTI <jasonsuryaf@gmail.com>
2022-07-27 15:16:42 +0700

    Add table

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="<?= base_url('assets/js/axios.min.js') ?>" defer></script>
+    <script src="<?= base_url('assets/js/axios.min.js') ?>"></script>
     <title>Coding Test</title>
 </head>
 <body>
@@ -30,7 +30,47 @@
             <button type="submit">Submit</button>
         </div>
     </form>
+    <table>
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>NIM</th>
+                <th>Nama</th>
+                <th>No. HP</th>
+                <th>Jenis Kelamin</th>
+            </tr>
+        </thead>
+        <tbody id="table-body">
+            
+        </tbody>
+    </table>
     <script>
+        const tableBody = document.getElementById('table-body');
+
+        function appendRow(i, data){
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
+                <td>${i}</td>
+                <td>${data.nim}</td>
+                <td>${data.nama}</td>
+                <td>${data.hp}</td>
+                <td>${data.jk === 'pria' ? 'Pria' : 'Wanita'}</td>
+            `;
+            tableBody.appendChild(tr);
+        }
+
+        function getData(){
+            tableBody.innerHTML = '';
+            axios.get('<?= base_url('mahasiswa') ?>')
+                .then((res) => {
+                    for(let i = 0; i < res.data.length; i++){
+                        appendRow(i + 1, res.data[i]);
+                    }
+                });
+        }
+
+        getData();
+
         document.getElementById('form-mahasiswa').addEventListener('submit', function(e){
             e.preventDefault();
             const target = e.target;
@@ -44,6 +84,7 @@
 
             axios.post('<?= base_url('mahasiswa') ?>', jsonObject)
                 .then((res) =>{
+                    getData();
                     target.querySelector('[name=\'nim\']').value =
                     target.querySelector('[name=\'nama\']').value =
                     target.querySelector('[name=\'hp\']').value = '';
